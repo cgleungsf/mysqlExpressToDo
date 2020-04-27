819b19801559cbfa112f4f19023f28aeabb399dd
Christina Leung <cgleungsf@gmail.com>
2020-04-27 10:40:55 -0700

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,18 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+let connection;
+
+// production connection - variable that only exist in production 
+if( process.env.JAWSDB.URL){
+connection = mysql.createConnection(process.env.JAWSDB_URL).promise();
+} 
+// local host conenction
+else {
+  connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password',
   database: 'todos_db',
 }).promise();
-
+}
 module.exports = connection;
