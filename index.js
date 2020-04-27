47228616a7dc5bd11a9d4873a2f2369b5fa7a7cc
Christina Leung <cgleungsf@gmail.com>
2020-04-27 11:49:57 -0700

    create front end

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 // Any route that goes to slash,
 // Have the router object inside of routes
 // handle the routing for us
