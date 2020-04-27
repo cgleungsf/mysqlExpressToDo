9d03640c03831f97c1f5fd2e4fd8f88b058fd407
Christina Leung <cgleungsf@gmail.com>
2020-04-27 10:48:12 -0700

    fixed port for heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 // Node will automatically look for an index.js inside of that folder
 const routes = require('./routes');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
